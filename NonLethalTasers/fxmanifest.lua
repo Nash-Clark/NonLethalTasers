2363ce4acf4facba4226cbe6bcdd4725d4b10f2a
Nash_Clark <120993028+Nash-Clark@users.noreply.github.com>
2023-02-01 17:18:30 -0600

    Create fxmanifest.lua

---
+++ NonLethalTasers/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+author 'Nash'
+
+version '1.0'
+
+description 'Make Tasers Non-Lethal'
+
+files {
+  'weapons.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
